6cbbe59b6353df5ef79367ddbc73c714bbd6320c
PrintTrd <60070034@kmitl.ac.th>
2020-04-13 22:20:46 +0700

    try to change ascii

--- gentext.js
+++ gentext.js
@@ -1,43 +1,81 @@
 /* Get the text field */
 var text = document.getElementById("myInput");
+var string = text.value;
+var newtext = document.getElementById("myOutput");
+const arr = string.split("");
 
 function copyText() {
   /* Select the text field */
-  text.select();
-  text.setSelectionRange(0, 99999); /*For mobile devices*/
+  newtext.select();
+  newtext.setSelectionRange(0, 99999); /*For mobile devices*/
 
-  /* Copy the text inside the text field */         document.execCommand("copy");
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
 
   /* Alert the copied text */
-  alert("Copied the text: " + text.value);
+  alert("Copied the text: " + newtext.value);
 }
 
 
-function spacing() {
-    text.style.letterSpacing = ".5em";
+function spacing(unicodearray) {
+    //var i=0; i<text.length; i++
+    for (var i in unicodearray){
+        //newtext.value.charAt(i) = text.value.charAt(i)+" ";
+        arr[i] = text.charCodeAt(i);
+        console.log(arr[i]);
+        console.log(text.charCodeAt(i));
+        //document.write(text.charCodeAt(i)+"\u00a0");
+        newtext.value += text.fromCharCode(unicodearray[i]);//+"\u00a0"; //&nbsp;
+        arr.push(newtext);
+    }
+    // newtext.value = text.value;
+    // newtext.style.letterSpacing = ".5em";
+
+    //var ch[] = text.split("");
+    //text.charAt[0];
+    //console.log(text.charAt[0]);
+    //var char = text.split(/(\w+)/);
+    //text.replace(/(\w)/g, '$1 ').replace(/(^\xa0+$)/,'');
+    // for (int i=0; i<ch.length; i++) {
+    //     // text.replace(text.charAt(i+1), `&nbsp;+${text.charAt(i + 1)}`);
+    //     // var char[i] = text[i]
+    //     // text.charAt(i+1);
+    //     if(ch[i] != " ") {
+    //         console.log(char[i]+" ");
+    //     }
+    //     //text.charAt[i];
+    // }
 }
 function lowercase() {
-    text.style.textTransform = "lowercase";
+    newtext.value = text.value;
+    newtext.style.textTransform = "lowercase";
 }
 function uppercase() {
-    text.style.textTransform = "uppercase";
+    newtext.value = text.value;
+    newtext.style.textTransform = "uppercase";
 }
 function bold() {
+    newtext.value = text.value;
     //Change the font weight style to 700
-    text.style.fontWeight = 'bold';
+    newtext.style.fontWeight = 'bold';
 }
 function impact() {
-    text.style.fontFamily = "Impact,Charcoal,sans-serif";
+    newtext.value = text.value;
+    newtext.style.fontFamily = "Impact,Charcoal,sans-serif";
 }
 function courier() {
-    text.style.fontFamily = "Courier New, Courier, monospace";
+    newtext.value = text.value;
+    newtext.style.fontFamily = "Courier New, Courier, monospace";
 }
 function allura() {
-    text.style.fontFamily = "Allura";
+    newtext.value = text.value;
+    newtext.style.fontFamily = "Allura";
 }
 function amatic() {
-    text.style.fontFamily = "Amatic SC";
+    newtext.value = text.value;
+    newtext.style.fontFamily = "Amatic SC";
 }
 function barrio() {
-    text.style.fontFamily = "Barrio";
+    newtext.value = text.value;
+    newtext.style.fontFamily = "Barrio";
 }
